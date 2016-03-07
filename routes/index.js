021489d53e59f6bf36208da13d12de9bb4c9144f
smilecs <mumene@gmail.com>
2016-03-07 13:16:06 +0100

    added login for all

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('home', { title: 'Express' });
+});
+
+router.get('/patient', function(req, res, next) {
   res.render('login', { title: 'Express' });
 });
 
@@ -10,8 +14,12 @@ router.get('/user/:id?', function(req, res, next) {
   res.render('client', { title: 'Express' });
 });
 
-router.get('/admin', function(req, res, next) {
+router.post('/admin', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/ad', function(req, res, next) {
+  res.render('adlogin', { title: 'Express' });
 });
 
 
